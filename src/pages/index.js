dd7a0511ee668bed6793f81c3f3e212d6f40a13f
Your Name <rghzaiel@ggn-group.com>
2023-06-07 12:55:06 +0100

    register user

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { HomePage } from "../pages/Home/HomePage";
 export { ProductsList } from "../pages/Products/ProductsList";
 export { ProductDetail } from "./ProductDetail";
+export { Login } from "./Login";
+export { Register } from "./Register";
--- src/routes/Allroutes.js
+++ src/routes/Allroutes.js
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage, ProductDetail, ProductsList } from "../pages";
+import {
+  HomePage,
+  Login,
+  ProductDetail,
+  ProductsList,
+  Register,
+} from "../pages";
 
 export const Allroutes = () => {
   return (
     <div className="main">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="products" element={<ProductsList />} />
+        <Route path="products/:id" element={<ProductDetail />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </div>
   );
