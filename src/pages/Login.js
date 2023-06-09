ca0be5db5a5e8eeff92f06756eec33652afa14b5
Your Name <rghzaiel@ggn-group.com>
2023-06-09 14:07:12 +0100

    login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,6 +26,26 @@ export const Login = () => {
       });
     }
   }
+
+  const handleLoginGuest = async () => {
+    email.current.value = "demo@gmail.com";
+    password.current.value = "learnreact";
+
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      toast.error(error.message, {
+        closeButton: true,
+        position: "bottom-center",
+      });
+    }
+  };
+
   return (
     <main>
       <section>
@@ -73,7 +93,12 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button
+        onClick={handleLoginGuest}
+        className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Login As Guest
+      </button>
     </main>
   );
 };
